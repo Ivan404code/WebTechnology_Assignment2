49899c1258cd18464d633b3469372e1193a098b0
IvanM <43602535+Ivan404code@users.noreply.github.com>
2019-05-17 15:39:34 +0100

    Create css.py
    This code shows the styles used for the website.

---
+++ css/css.py
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Clean Water</title>
+<link href="css_styles/style.css" rel="stylesheet">
+</head>
+<body>
+     <header>
+        <div class = "row">
+            <div class="logo">
+                <img src="images/logo.png" alt="The company logo">
+            </div>
+            <ul class = "main-nav">
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="">EVENTS</a></li>
+            <li><a href="">ABOUT US</a></li>
+            <li><a href="">CONTACT US</a></li>
+                
+            </ul>
+        </div>
+        <div class="text">
+            <h1>YOU CAN MAKE A DIFFERENCE</h1>
+                <div class="button">
+                    <a href="registration.html" class="btn-one">JOIN US</a>
+                    <a href="" class="btn-two">DONATE</a>
+                    
+                </div>
+                
+            
+        </div>
+    </header>
+    <div class="copyright">
+        <footer>
+        <small>&copy; Copyright 2019, Ivan</small>
+        </footer>
+    </div>
+    
+</body>
