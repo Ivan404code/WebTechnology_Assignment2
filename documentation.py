c441b5bb5aea67e880a14c6d965eca5db11bf011
IvanM <43602535+Ivan404code@users.noreply.github.com>
2019-05-17 15:41:52 +0100

    Create documentation.py

---
+++ documentation.py
@@ -0,0 +1,49 @@
+# 1.1 Documentation
+
+## 1.1.1 Background
+
+The aim of this project is to publicize and promote a real life problem, in
+this case, poluted rivers, lakes and others, by creating a responsive website.
+The website will focus on promoting campaigns and different types of 
+events that volunteers will take part in to clean certain riverssides that 
+are severely poluted. In addition to that the website will have the function
+to accept donations that will help acquire the needed equpment to purify water
+and clean river sides.
+
+The main idea of the campaign is that people voluntarily agree to participate
+in the different events. Because of that, sharing this problem online is a 
+great idea for advertisement. This is a great way to get a bigger number
+of people to see and discuss a certain problem. Another big reason is that 
+all of the people that are already interested, might want to learn more about
+the organisation and its' campaigns. What better way than displaying all of 
+the relevant and important information on the website. That way everyone
+can get educated about business of the company without having to spend 
+a great amount of research.
+
+Last but not least, the website will serve as a comminity platform, where 
+people can join and receive information about upcomming events,which will help
+them be more active and talk openly about anything concerning the clenliness
+of local river.
+## 1.1.2 Wireframes
+![Desktop home] (./wireframes/Desktop_home.jpg)
+![Desktop member] (./wireframes/Desktop_member.jpg)
+![Mobile_home] (./wireframes/Mobile_home.jpg)
+![Mobile member] (./wireframes/Mobile_member.jpg)
+
+## 1.1.3 Color scheme
+The colors used were mainly blue-#0000FF, white-#FFFFFF, green-#008000.
+The reason behind the use of these colors was to make a feeling clenliness, 
+also these colors are mainly seen in nature, like trees, rivers and others , 
+thats why the user will get a good idea of what the website offers and what
+it promotes.
+
+## 1.1.4 Accessibility
+
+Accessibility is a very important issue to be accounted nowadays in websites,
+since not all are able to see or hear elements from a certain website.
+In my website I included an "alt" attribute which helps blind people listen,
+to a short description of a given image, in this case the logo.
+
+## Link to GitHub 
+----https://github.com/Ivan404code/WebTechnology_Assignment2----
+
